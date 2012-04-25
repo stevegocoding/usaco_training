0cb0c30faa9a928e935dff2328ef4b8ddfe8514c
Steve Shi <stevegocoding@gmail.com>
2012-04-25 17:18:06 -0400

    pass friday problem!

--- src/friday.cpp
+++ src/friday.cpp
@@ -41,10 +41,14 @@ public:
     bool is_leap_year()
     {
         int frac = m_year % 400;
-        if (frac == 0) 
+        if (frac == 0)
             return true;
         else
         {
+            frac = m_year % 100;
+            if (frac == 0) 
+                return false; 
+ 
             frac = m_year % 4; 
             if (frac == 0)
                 return true;
@@ -52,32 +56,35 @@ public:
                 return false; 
         }
     }
-    
-    void next_month()
+
+    void next_day()
     {
-        unsigned int delta_days = 0; 
+        unsigned int month_days = 0; 
         if (m_month == 1)
         {
             if (is_leap_year())
             {
-                delta_days = 29; 
+                month_days = 29; 
             }
             else 
             {
-                delta_days = 28; 
+                month_days = 28; 
             }
         }
         else if (m_month == 3 || m_month == 5 || m_month == 8 || m_month == 10)
         {
-            delta_days = 30; 
+            month_days = 30; 
         }
         else 
-            delta_days = 31;
+            month_days = 31;
+
+        unsigned int delta_month = (m_day + 1) / month_days;
+        unsigned int delta_year = (m_month + delta_month) / 12;
         
-        unsigned int delta_week_day = delta_days % 7; 
-        m_week_day = (m_week_day + delta_week_day) % 7;
-        m_month = (m_month + 1) % 12;
-        m_year += (m_month + 1) / 12; 
+        m_week_day = (m_week_day + 1) % 7;
+        m_day = (m_day + 1) % month_days; 
+        m_month = (m_month + delta_month) % 12;
+        m_year = m_year + delta_year;
     }
 
     unsigned int get_year() { return m_year; }
@@ -99,7 +106,7 @@ int main(int argc, char **argv)
     unsigned int years = 0; 
     ifs >> years; 
     
-    c_date date(2000, JAN, 12, THU); 
+    c_date date(1900, JAN, 0, MON); 
     unsigned int mon_13th = 0;
     unsigned int tue_13th = 0; 
     unsigned int wed_13th = 0; 
@@ -107,38 +114,42 @@ int main(int argc, char **argv)
     unsigned int fri_13th = 0; 
     unsigned int sat_13th = 0; 
     unsigned int sun_13th = 0; 
-    while ((date.get_year() - 2000) < years)
+    while ((date.get_year() - 1900) < years)
     {
-        date.next_month();
+        date.next_day();
         
-        unsigned int wd = date.get_week_day(); 
-        switch (wd)
+        if (date.get_day() == 12) 
         {
-        case 0:
-            mon_13th++; 
-            break;
-        case 1:
-            tue_13th++;
-            break;
-        case 2:
-            wed_13th++; 
-            break; 
-        case 3:
-            thu_13th++; 
-            break;
-        case 4: 
-            fri_13th++; 
-            break;
-        case 5:
-            sat_13th++; 
-            break; 
-        case 6:
-            sun_13th++; 
-            break;
+            unsigned int wd = date.get_week_day(); 
+
+            switch (wd)
+            {
+            case 0:
+                mon_13th++; 
+                break;
+            case 1:
+                tue_13th++;
+                break;
+            case 2:
+                wed_13th++; 
+                break; 
+            case 3:
+                thu_13th++; 
+                break;
+            case 4: 
+                fri_13th++; 
+                break;
+            case 5:
+                sat_13th++; 
+                break; 
+            case 6:
+                sun_13th++; 
+                break;
+            }
         }
     }
     
-    ofs << mon_13th << " " << tue_13th << " " << wed_13th << " "; 
-    ofs << thu_13th << " " << fri_13th << " " << sat_13th << " " << sun_13th; 
+    ofs << sat_13th << " " << sun_13th << " " << mon_13th << " " << tue_13th << " " << wed_13th << " " << thu_13th << " " << fri_13th; 
+    ofs << std::endl; 
     return 0;
 }
