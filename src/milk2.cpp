ba8a2025d05b65967c79beaabbb3583b8ada8c83
Steve Shi <stevegocoding@gmail.com>
2012-04-30 12:20:20 -0400

    passed milk2 problem!

--- src/milk2.cpp
+++ src/milk2.cpp
@@ -10,22 +10,26 @@ PROG: milk2
 
 using namespace std; 
 
-struct milk_interval
+enum time_point_type 
 {
-    unsigned int start; 
-    unsigned int end;
-
-    milk_interval(unsigned int _start = 0, unsigned int _end = 0) 
-        : start(_start)
-        , end(_end)
-    {}
-
-    void merge(const milk_interval& interval)
-    {
-        start = min(start, interval.start); 
-        end = max(end, interval.end); 
-    }
+    MILK_START = 0,
+    MILK_END
 };
+
+struct time_point 
+{
+    unsigned int time; 
+    time_point_type t;
+    time_point(unsigned int _time, time_point_type _t)
+        : time(_time)
+        , t(_t)
+    {} 
+};
+
+bool time_point_comp(const time_point& t1, const time_point& t2)
+{
+    return (t1.time < t2.time) || (t1.time == t2.time && t1.t < t2.t); 
+}
 
 int main(int argc, char **argv)
 {
@@ -34,70 +38,51 @@ int main(int argc, char **argv)
     
     int num_cows = 0; 
     ifs >> num_cows; 
-    vector<milk_interval> intervals; 
-    intervals.reserve(num_cows);
     
-    for (int i = 0; i < num_cows; ++i)
+    vector<time_point> time_points; 
+    time_points.reserve(num_cows * 2); 
+    
+    for (int i = 0; i < num_cows * 2; ++i)
     {
-        unsigned int start = 0;
-        unsigned int end = 0;
-        ifs >> start >> end;
-        intervals.push_back(milk_interval(start, end));
+        unsigned int time;
+        ifs >> time; 
+        time_point_type t = (time_point_type)(i % 2);
+        time_points.push_back(time_point(time, t));
     }
     
-    vector<milk_interval>::iterator it = intervals.begin(); 
-    milk_interval longest = *it;
-    milk_interval temp = *it; 
-    int longest_no_milk = 0; 
-    int temp2 = 0; 
-    unsigned int minimum = 100000; 
-    unsigned int maximum = 0; 
-    for (; it != intervals.end(); ++it)
+    sort(time_points.begin(), time_points.end(), time_point_comp); 
+    
+    vector<time_point>::iterator it = time_points.begin(); 
+    int longest_in_milk_interval = 0;
+    int longest_non_milk_interval = 0;
+    int in_milk_cnt = 0; 
+    int milk_start_time = 0;
+    int milk_end_time = 0;
+    for(; it != time_points.end(); ++it)
     {
-        /*
-        if ((temp2 = (it->start - temp.end)) > 0)
+        if (it->t == MILK_START)
         {
-            if (temp.end - temp.start > longest.end - longest.start)
-                longest = temp;
-            temp = *it; 
-            longest_no_milk = max(temp2, longest_no_milk);
-            continue;
-        } 
-        else 
-        {
-            if (it->start <= longest.)
-                temp.merge(*it); 
+            if (in_milk_cnt == 0) 
+            {
+                milk_start_time = it->time; 
+                if (it != time_points.begin() && milk_start_time - milk_end_time > longest_non_milk_interval)
+                    longest_non_milk_interval = milk_start_time - milk_end_time; 
+            }
+            in_milk_cnt++; 
         }
-        */
-
-        int d0 = it->start - temp.end; 
-        int d1 = temp.start - it->end; 
-        temp2 = max(d0, d1); 
-        minimum = min(minimum, it->start); 
-        maximum = max(maximum, it->end); 
-        if (d0 > 0 || d1 > 0)
+        else if (it->t == MILK_END)
         {
-            if (temp.end - temp.start > longest.end - longest.start)
-                longest = temp;
-            if (temp2 > longest_no_milk)
-                longest_no_milk = temp2; 
-            temp = *it;
-            continue;
-        }
-        else 
-        {
-            temp.merge(*it);
-            if (temp.end - temp.start > longest.end - longest.start)
-                longest = temp;
-        }
+            in_milk_cnt--; 
+            if (in_milk_cnt == 0)
+            {
+                milk_end_time = it->time; 
+                if (milk_end_time - milk_start_time > longest_in_milk_interval)
+                    longest_in_milk_interval = milk_end_time - milk_start_time;
+            }
+        }    
     }
-
-    if (longest.start <= minimum && longest.end >= maximum) 
-        longest_no_milk = 0; 
-   
-    ofs << longest.end-longest.start << " "; 
-    ofs << longest_no_milk;
-    ofs << endl; 
-
+    
+    ofs << longest_in_milk_interval << " " << longest_non_milk_interval << endl; 
+    
     return 0; 
 }
